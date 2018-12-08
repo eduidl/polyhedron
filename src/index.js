7868668346a058235850ab9c68d1cf2289ee2da9
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2018-12-09 05:18:37 +0900

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-const THREE = require('three');
-const TrackballControls = require('three-trackballcontrols');
-
-import { POINTS, EDGES } from './data.js';
+import ThreeJS from '../lib/three_js.js'
+import { POINTS, EDGES } from '../lib/data.js';
 
 const getUrlQuery = () => {
   let query_params = {};
@@ -13,66 +11,24 @@ const getUrlQuery = () => {
   return query_params;
 };
 
+function render() {
+  requestAnimationFrame(render);
+  three_js.render();
+}
+
 const index = parseInt(getUrlQuery()['index'] || '0');
 document.querySelector(`[data-index="${index}"]`).classList.add('active');
 
-const points = POINTS[index];
-const edges = EDGES[index];
-
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-const scene = new THREE.Scene();
-
-// light
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(0, 100, 30);
-scene.add(light);
-const ambient = new THREE.AmbientLight(0x404040);
-scene.add(ambient);
-
-// camera
-const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
-camera.position.set(20, 10, 30);
-camera.lookAt(scene.position);
-
-// controls
-const controls = new TrackballControls(camera);
-controls.rotateSpeed = 5.0;
-
-// renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(width, height);
-renderer.setClearColor(0xffffff);
-renderer.setPixelRatio(window.devicePixelRatio);
-document.getElementById('stage').appendChild(renderer.domElement);
-
-function render() {
-  requestAnimationFrame(render);
-  controls.update();
-  renderer.render(scene, camera);
-}
-
-edges.forEach(function (edge) {
-  const geom = new THREE.Geometry();
-  geom.vertices.push(new THREE.Vector3(points[edge[0]][0],
-                                       points[edge[0]][1],
-                                       points[edge[0]][2]));
-  geom.vertices.push(new THREE.Vector3(points[edge[1]][0],
-                                       points[edge[1]][1],
-                                       points[edge[1]][2]));
-  const line = new THREE.Line(geom, new THREE.LineBasicMaterial({ color: 'green' }));
-  scene.add(line);
-});
-
-const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(9, 30, 30),
-  new THREE.MeshBasicMaterial({
-    transparent: true,
-    opacity: 0.7,
-    color: 0xffffff
-  })
-);
-scene.add(sphere);
-
+const three_js = new ThreeJS();
+three_js.initialize(EDGES[index], POINTS[index]);
 render();
+
+let timer = 0;
+window.addEventListener('resize', () => {
+  if (timer > 0) clearTimeout(timer);
+  timer = setTimeout(() => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    three_js.resize(width, height);
+  }, 200);
+}, false);
